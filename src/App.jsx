7581f5f09908c522a189eb49883139da1e64f940
hassan lawal <hassanlawalkdurbi@gmail.com>
2023-12-21 16:54:20 +0100

    portal

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Signin from "./assets/Components/Signin"
+
+function App() {
+
+
+  return (
+    <>
+    <Signin/>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/Components/Signin.jsx
@@ -0,0 +1,22 @@
+import "../Styles/Signin.css";
+const Signin = () => {
+    return ( 
+        <>
+        
+    
+        <h5>Welcome To Hub portal</h5>
+        <p>It is long edtablished that a reader will be disttacted by the <br/>readablecontenct 0f a page when looking at its layout </p>
+        <div>
+        <button className="one">Sign up</button>
+        <button className="two">Sign in</button>
+        </div>
+        
+    
+        </>
+    
+        
+    
+     );
+}
+ 
+export default Signin;
